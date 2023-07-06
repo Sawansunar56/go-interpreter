3e43335ab55e1a4b9056efe070374c1124bf1ac5
Sawan Sunar <sawansunar24072002@gmail.com>
2023-07-06 18:58:47 +0530

    Second day: extended the test

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,7 +1,7 @@
 package lexer
 
 import (
-  "example/sawan/goInterpreter/token"
+	"example/sawan/goInterpreter/token"
 )
 
 type Lexer struct {
@@ -12,7 +12,56 @@ type Lexer struct {
 }
 
 func (l *Lexer) NextToken() token.Token {
-  var tok token.Token
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACES, l.ch)
+	case '}':
+		tok = newToken(token.RBRACES, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+  default:
+    if isLetter(l.ch) {
+      tok.Literal = l.readIdentifier()
+      return tok
+    } else {
+      tok = newToken(token.ILLEGAL, l.ch)
+    }
+	}
+
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+  position := l.position
+
+  for isLetter(l.ch) {
+    l.readChar()
+  }
+  return l.input[position: l.position]
+}
+
+func isLetter(ch byte) bool {
+  return 'a' <= ch && ch <= 'z' || 'A' <= ch && 'Z' <= ch || ch == '_'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
 func New(input string) *Lexer {
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,23 +2,60 @@ package lexer
 
 import (
 	"example/sawan/goInterpreter/token"
+	"fmt"
 	"testing"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+  let ten = 10;
+
+  let add = fn(x,y) {
+    x + y;
+  };
+
+  let result = add(five, ten);
+  `
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACES, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACES, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
 		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
@@ -28,6 +65,7 @@ func TestNextToken(t *testing.T) {
 	for i, tt := range tests {
 		tok := l.NextToken()
 
+		fmt.Printf("%d, expectedType=%q, got=%q\n", i, tt.expectedType, tok.Type)
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokenType wrong, expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
