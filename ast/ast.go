aeacdb779749c20a3bf7b24c7c35c146179c1ee4
Sawan Sunar <sawansunar24072002@gmail.com>
2023-07-18 15:45:09 +0530

    Parser Ongoing

--- ast/ast.go
+++ ast/ast.go
@@ -4,24 +4,35 @@ import (
 	"example/sawan/goInterpreter/token"
 )
 
+// Every interface must implement this node so that they implement the token literal and have a string
 type Node interface {
 	TokenLiteral() string
 }
 
+// Statements produce no value while Expressions produce value
+// Expressions produce value but Statements don't
+// e.g - For monkey Language, let is a statement, and inside it variable name is an Identifier and the value is the expression
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// Expressions produce value but Statements don't
+// e.g - For monkey Language, let is a statement, and inside it variable name is an Identifier and the value is the expression
 type Expression interface {
 	Node
 	expressionNode()
 }
 
+// Program node is going to be the root node of every AST our parser produces
+// Every valid Monkey program is a series of statments. These statements are
+// contained in the Program.Statements, which is a slice of AST nodes that implement
+// the Satement interface.
 type Program struct {
 	Statements []Statement
 }
 
+// Returns the first statement from the program.
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -40,9 +51,17 @@ func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 type Identifier struct {
-  Token token.Token
-  Value string
+	Token token.Token
+	Value string
 }
 
-func (i *Identifier) expressionNode() {}
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"example/sawan/goInterpreter/ast"
 	"example/sawan/goInterpreter/lexer"
 	"example/sawan/goInterpreter/token"
+	"fmt"
 )
 
 /*
@@ -14,26 +15,101 @@ peekToken - holds the positon of the next token
 type Parser struct {
 	l *lexer.Lexer
 
+  errors []string
+
 	curToken  token.Token
 	peekToken token.Token
 }
 
-// creates a new parser with default values
+// creates a new parser with default values and returns it
 func New(l *lexer.Lexer) *Parser {
-  p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
-  p.nextToken()
-  p.nextToken()
-  
-  return p
+	p.nextToken()
+	p.nextToken()
+
+	return p
 }
+
+func (p *Parser) Errors() []string {
+  return p.errors
+}
+
 
 // helper function to move forward in a parser
 func (p *Parser) nextToken() {
-  p.curToken = p.peekToken
-  p.peekToken = p.l.NextToken()
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
 }
 
+// Makes the root program and then adds every statement into it.
+//
+// Iterates over the tokens until it meets and EOF token and goes through the token
+// the nextToken Method
 func (p *Parser) ParseProgram() *ast.Program {
-  return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// checks whether the current value is the provided token
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+    p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+  msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+  p.errors = append(p.errors, msg)
 }
